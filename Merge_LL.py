8a6118ce334dfb8c527bb095a4731ba15ec87825
chvkanishk <98982721+chvkanishk@users.noreply.github.com>
2024-09-29 18:01:21 -0700

    Merge and sorted Linked List

--- Merge_LL.py
+++ Merge_LL.py
@@ -1,5 +1,44 @@
 from Linked_List import LinkedList
 
-l=LinkedList()
-l.append(1)
-l.Display()
+
+
+def Merge(l1,l2):
+    merged_list= LinkedList()
+
+    c1=l1.head
+    c2= l2.head
+    while c1 and c2:
+        if c1.data <= c2.data:
+            merged_list.append(c1.data)
+            c1=c1.next
+        else:
+            merged_list.append(c2.data)
+            c2= c2.next
+    
+    while c1:
+        merged_list.append(c1.data)
+        c1= c1.next
+    while c2:
+        merged_list.append(c2.data)
+        c2= c2.next
+    
+    return merged_list
+
+
+
+
+list1= LinkedList()
+list2 = LinkedList()
+
+list1.append(1)
+list1.append(2)
+list1.append(4)
+
+list2.append(1)
+list2.append(3)
+list2.append(4)
+
+mergell= LinkedList()
+mergell=Merge(list1,list2)
+mergell.Display()
+
